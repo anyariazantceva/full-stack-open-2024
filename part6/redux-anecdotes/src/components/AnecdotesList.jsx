7c2140990f4a93f9e367ab0634c7072006f94129
Anna Riazantseva <anyariazantzeva@yandex.ru>
2025-04-29 19:44:30 +0300

    6.9

--- part6/redux-anecdotes/src/components/AnecdotesList.jsx
+++ part6/redux-anecdotes/src/components/AnecdotesList.jsx
@@ -1,8 +1,19 @@
 import { useSelector, useDispatch } from "react-redux";
 import { voteForAnecdote } from "../reducers/anecdoteReducer";
+import Filter from "../components/Filter";
 
 const AnecdotesList = () => {
-  const anecdotes = useSelector((state) => state);
+  const anecdotes = useSelector((state) => {
+    if (state.filter === "") {
+      return state.anecdotes;
+    } else {
+      return state.anecdotes.filter((anecdote) => {
+        return anecdote.content
+          .toLowerCase()
+          .includes(state.filter.toLowerCase());
+      });
+    }
+  });
   const dispatch = useDispatch();
 
   const vote = (id) => {
@@ -12,6 +23,7 @@ const AnecdotesList = () => {
   return (
     <div>
       <h2>Anecdotes</h2>
+      <Filter />
       {anecdotes.map((anecdote) => (
         <div key={anecdote.id}>
           <div>{anecdote.content}</div>
