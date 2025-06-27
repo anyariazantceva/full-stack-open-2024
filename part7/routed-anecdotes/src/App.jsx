5310adef628ad31d9b4169136134ed350a676852
Anna Riazantseva <anyariazantzeva@yandex.ru>
2025-06-27 15:24:20 +0300

    7.5

--- part7/routed-anecdotes/src/App.jsx
+++ part7/routed-anecdotes/src/App.jsx
@@ -93,6 +93,13 @@ const CreateNew = (props) => {
     });
   };
 
+  const handleReset = (e) => {
+    e.preventDefault();
+    content.onReset();
+    author.onReset();
+    info.onReset();
+  };
+
   return (
     <div>
       <h2>create a new anecdote</h2>
@@ -118,6 +125,7 @@ const CreateNew = (props) => {
           <input name="info" value={info.value} onChange={info.onChange} />
         </div>
         <button>create</button>
+        <button onClick={handleReset}>reset</button>
       </form>
     </div>
   );
