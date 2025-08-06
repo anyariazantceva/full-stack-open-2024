9c091500166fce0104e51834b6066ab0507767be
Anna Riazantseva <anyariazantzeva@yandex.ru>
2025-08-06 14:46:10 +0300

    8.7

--- part8/books/index.js
+++ part8/books/index.js
@@ -124,6 +124,10 @@ const typeDefs = `
             name: String!
             born: Int
         ) : Author
+        editAuthor(
+            name: String
+            setBornTo: Int!
+        ) : Author
     }
 
   type Query {
@@ -177,6 +181,16 @@ const resolvers = {
             const book = { ...args, id: uuid() }
             books = books.concat(book)
             return book
+        },
+        editAuthor: (root, args) => {
+            const author = authors.find(a => a.name === args.name)
+            if (!author) {
+                return null
+            }
+
+            const updatedAuthor = { ...author, born: args.setBornTo }
+            authors = authors.map(a => a.name === args.name ? updatedAuthor : a)
+            return updatedAuthor
         }
     }
 }
