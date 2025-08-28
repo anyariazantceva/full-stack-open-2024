5e15e6a5e38804c81cff45976317dec8b4130d04
Anna Riazantseva <anyariazantzeva@yandex.ru>
2025-08-28 08:35:17 +0300

    8.17-8.18

--- part8/library-frontend/src/App.jsx
+++ part8/library-frontend/src/App.jsx
@@ -2,10 +2,14 @@ import { useState } from "react";
 import Authors from "./components/Authors";
 import Books from "./components/Books";
 import NewBook from "./components/NewBook";
+import LoginForm from "./components/LoginForm";
+import { useApolloClient } from "@apollo/client";
 
 const App = () => {
   const [page, setPage] = useState("authors");
+  const [token, setToken] = useState(null);
   const [errorMessage, setErrorMessage] = useState(null);
+  const client = useApolloClient();
   const notify = (message) => {
     setErrorMessage(message);
     setTimeout(() => {
@@ -13,22 +17,38 @@ const App = () => {
     }, 10000);
   };
 
-  return (
-    <div>
+  const logout = () => {
+    setToken(null);
+    localStorage.clear();
+    client.resetStore();
+  };
+
+  if (!token) {
+    return (
+      <>
+        <Notify errorMessage={errorMessage}></Notify>
+        <LoginForm setToken={setToken} setError={notify} />
+      </>
+    );
+  } else {
+    return (
       <div>
-        <button onClick={() => setPage("authors")}>authors</button>
-        <button onClick={() => setPage("books")}>books</button>
-        <button onClick={() => setPage("add")}>add book</button>
+        <div>
+          <button onClick={() => setPage("authors")}>authors</button>
+          <button onClick={() => setPage("books")}>books</button>
+          <button onClick={() => setPage("add")}>add book</button>
+          <button onClick={logout}>logout</button>
+        </div>
+        <Notify errorMessage={errorMessage}></Notify>
+
+        <Authors show={page === "authors"} />
+
+        <Books show={page === "books"} setError={notify} />
+
+        <NewBook show={page === "add"} />
       </div>
-      <Notify errorMessage={errorMessage}></Notify>
-
-      <Authors show={page === "authors"} />
-
-      <Books show={page === "books"} setError={notify} />
-
-      <NewBook show={page === "add"} />
-    </div>
-  );
+    );
+  }
 };
 
 const Notify = ({ errorMessage }) => {
